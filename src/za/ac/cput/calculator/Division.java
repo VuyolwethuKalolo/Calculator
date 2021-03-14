e4a7e6bf5ef483d40d9b8b9b2e4f4c90a26d1142
Vuyolwethu <214015637@mycput.ac.za>
2021-03-14 10:34:31 -0700

    First Commit

---
+++ src/za/ac/cput/calculator/Division.java
@@ -0,0 +1,15 @@
+package za.ac.cput.calculator;
+
+public class Division {
+    /**
+     *  Returns results of integer division
+     * @param numerator
+     * @param denominator
+     * @return
+     */
+    public static int integerDivision(int numerator, int denominator){
+     if (denominator <1) return 0;
+     return numerator/denominator;
+
+    }
+}
